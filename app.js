43980e54f5bf4dbbf01df05044aa7b5a128c0626
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-11-13 09:49:16 +0000

    Schema for adding post defined

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var express = require("express"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
     app     = express();
     
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/travelground");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var travelgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description:String
+
+});
+
+var Travelground = mongoose.model("Travelground",travelgroundSchema);
 
 app.get("/",function(req,res){
     res.render("index");
 });    
 
-app.get("/posts",function(req,res){
+app.get("/travelgrounds",function(req,res){
     res.redirect("/");
 });
 
