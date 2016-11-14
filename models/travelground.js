dd355e67c3d26fe90900779ec9e7a3f40484049a
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-11-14 06:16:53 +0000

    Travelground DB modified with author and date

--- models/travelground.js
+++ models/travelground.js
@@ -4,12 +4,20 @@ var travelgroundSchema = new mongoose.Schema({
     name: String,
     image: String,
     description:String,
+    author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
     comments: [
       {
          type: mongoose.Schema.Types.ObjectId,
          ref: "Comment"
       }
-   ]
+   ],
+   created:{ type:Date, default:Date.now }
 
 });
 //DB Model
