c7e90c186b15ef2d88ca64de82d0fa49d5d964d6
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-11-18 12:47:11 +0000

    Added background slider

---
+++ public/javascript/slider.js
@@ -0,0 +1,7 @@
+
+$.backstretch([
+    "https://images.unsplash.com/photo-1466854076813-4aa9ac0fc347?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=7fa1a4fd8fbc1ff1ba5ff2ae316da6c2",
+    "https://images.unsplash.com/photo-1468956398224-6d6f66e22c35?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=2ac1e19ac2e272fac58c0bb5430c4399"
+  , "https://images.unsplash.com/photo-1469461084727-4bfb496cf55a?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=20a1cc7283c543bb5d77211048c4df93"
+  , "https://images.unsplash.com/photo-1414016642750-7fdd78dc33d9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=807fec0ecaf8c55a13ae37196b790412"
+], {duration: 3000, fade: 1000});
